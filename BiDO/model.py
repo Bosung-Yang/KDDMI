e7de5ca1fc882b14c0da57c38719cb11e0da25d1
Bosung-Yang <yangbosung82@gmail.com>
2023-08-09 16:34:12 +0900

    vggvs

--- BiDO/model.py
+++ BiDO/model.py
@@ -381,7 +381,26 @@ class VGG16(nn.Module):
         out = F.softmax(res, dim=1)
 
         return out
-
+    
+class VGG16_(nn.Module):
+    def __init__(self, n_classes):
+        super(VGG16, self).__init__()
+        model = torchvision.models.vgg16_bn(pretrained=True)
+        self.feature = model.features
+        self.feat_dim = 512 * 2 * 2
+        self.n_classes = n_classes
+        self.bn = nn.BatchNorm1d(self.feat_dim)
+        self.bn.bias.requires_grad_(False)  # no shift
+        self.fc_layer = nn.Linear(self.feat_dim, self.n_classes)
+            
+    def forward(self, x):
+        feature = self.feature(x)
+        feature = feature.view(feature.size(0), -1)
+        feature = self.bn(feature)
+        res = self.fc_layer(feature)
+        
+        return [feature, res]
+    
 class VGG16_softmax(nn.Module):
     def __init__(self, n_classes, hsic_training=False, dp_training=False, dataset='celeba'):
         super(VGG16, self).__init__()
