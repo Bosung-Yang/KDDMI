343cf4d01d8412333d3b126301c68c43ae23a0c5
Bosung-Yang <yangbosung82@gmail.com>
2023-08-14 19:44:46 +0000

    edit docker setting

--- run_docker.py
+++ run_docker.py
@@ -2,4 +2,4 @@ import os
 
 os.system('sudo apt-get update ; sudo apt-get install nvidia-docker2 -y')
 os.system('sudo systemctl restart docker')
-os.system('sudo docker run -v ./data:/workspace/data -it --runtime=nvidia bosungyang/dmi:0809 bash')
+os.system('sudo docker run -v ./:/workspace/KDDMI -it --runtime=nvidia bosungyang/dmi:data bash')
