40c1a3321eddb39aadd750d8d7557e88d743437e
Bosung-Yang <139629920+Bosung-Yang@users.noreply.github.com>
2023-09-16 14:24:19 +0900

    Update run_docker.py

--- run_docker.py
+++ run_docker.py
@@ -2,4 +2,4 @@ import os
 
 os.system('sudo apt-get update ; sudo apt-get install nvidia-docker2 -y')
 os.system('sudo systemctl restart docker')
-os.system('sudo docker run -v ./:/workspace/KDDMI -it --shm-size=8G --runtime=nvidia bosungyang/dmi:data bash')
+os.system('sudo docker run -v ./:/workspace/KDDMI -it --shm-size=8G --runtime=nvidia --name bosung bosungyang/dmi:0907 bash')
