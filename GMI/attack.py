e7e361717ed48b411703b223d8716bd40f6c357f
Bosung-Yang <yangbosung82@gmail.com>
2023-09-30 14:23:10 +0900

    asd

--- GMI/attack.py
+++ GMI/attack.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
         ckp_E = torch.load(path_E)
         E_vgg.load_state_dict(ckp_E['state_dict'])
 
-        E_list = [(E_hsic, 'hisc'), (E_vib,'vib'), (E_vgg, 'vgg')]
+        E_list = [(E_hsic, 'hsic'), (E_vib,'vib'), (E_vgg, 'vgg')]
 
         
         g_path = "./G.tar"
